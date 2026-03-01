6a2dfe00f69c75394f827a33ac11a6a651b38dba
Hugo Gonzalez <gunnerctes@gmail.com>
2026-03-01 03:12:50 -0300

    Fix import App.jsx por Netlify build

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-import React from "https://esm.sh/react@19";
-import ReactDOM from "https://esm.sh/react-dom@19/client";
-import App from "../Aplicacion.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
